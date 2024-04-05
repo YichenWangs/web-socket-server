94cbe3df6f99ecdfe45a9df880ebe87729002b1c
Yichen Wang <yichenwang@Yichens-MacBook-Pro.local>
2024-04-05 17:33:03 +1100

    Initial code for handling AR server, and a ws client connects to genai_charles server. The communication between received ai message to AR server may need to have another look - not sure if it's connected

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/**
+ * Library for running websocket server and clients.
+ */
 const express = require('express');
 const { createServer } = require('http');
 const WebSocket = require('ws');
@@ -8,13 +11,15 @@ const { timeStamp, log } = require('console');
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-
 const port = 3000;
 const app = express();
 const server = createServer(app);
 const wss = new WebSocket.Server({ server });
 const sockets = {};
 
+/**
+ * A bunch of ip addresses handling devices in Yichen's collaborative AR study.
+ */
 const HL19_ip = "::ffff:192.168.0.172";
 const HL21_ip = "::ffff:192.168.0.253";
 const HL35_ip = "::ffff:192.168.0.50";
@@ -25,7 +30,6 @@ const ethernet1 = "::ffff:192.168.0.19";
 const ethernet2 = "::ffff:192.168.0.248";
 const ethernet3 = "::ffff:192.168.0.249";
 const charles_ai = "::ffff:192.168.0.81:5001";
-// const charles = "::ffff:192.168.0.81";
 
 var laptop1 = minilab1;
 var laptop2 = minilab2; 
@@ -34,8 +38,50 @@ var hl2 = ethernet3;
 var hl3 = ethernet1;
 var hl4 = ethernet2;
 
+/**
+ * Handy variables handling small things.
+ */
 var filename;
 var exp_con;
+
+var genai_data;
+
+/**
+ * Websocket Client that connects to charles genai_midi_module.
+ */
+const genai_ws = new WebSocket('ws://192.168.0.81:5001');
+
+genai_ws.on('open', function open() {
+  console.log("Connection o charles's genAI server is open.");
+  // Useless prompt message.
+  genai_ws.send('/channel/0/noteoff/0/0');
+});
+
+
+genai_ws.on('message', function message(data) {
+  console.log('Received from the AI agent: %s', data);
+  genai_data = data;
+  // Broadcast to hololens --- todo: does this work?
+  to(hl1, genai_data);
+  to(hl2, genai_data);
+
+
+});
+
+genai_ws.on('error', console.error);
+genai_ws.on('close', function close(data) {
+  console.log("The connection is closed.")
+  console.log("Error information" + data);
+  // setTimeout(() => {
+  //   connect();
+  // }, 1);
+});
+
+// sockets[genai_ws.id] = genai_ws; // add to the list of socket
+
+// genai_ws.on('message', route_message); // could do it this way so that
+
+// }
 
 
 const questions = [
@@ -61,89 +107,90 @@ inquirer.prompt(questions).then(answers => {
 });
 
 
+/**
+ * Directing to difference devices vis ip address + musical instrument data.
+ * @param {*} user 
+ * @param {*} data 
+ */
 // thanks to https://stackoverflow.com/questions/51316727/how-do-i-send-a-message-to-a-specific-user-in-ws-library.
 function to(user, data) {
     if(sockets[user] && sockets[user].readyState === WebSocket.OPEN)
         sockets[user].send(data);
 }
 
+/**
+ * Websocket Server on connection.
+ */
 wss.on('connection', function(ws, request, client) {
   console.log("client joined.");
   const user_id = request.socket.remoteAddress;
   ws.id = user_id;
   sockets[ws.id] = ws;
-  // console.log(ws);
+  // console.log(sockets);
 
-  //create new file
-  fs.open(filename, 'w', function (err, file) {
-    if (err) throw err;
-    console.log('File created!');
-  }); 
+  // //create new file
+  // fs.open(filename, 'w', function (err, file) {
+  //   if (err) throw err;
+  //   console.log('File created!');
+  // }); 
 
-  ws.on('error', console.error);
+
+
+  ws.on('error', function error (err){
+    console.log(err.code);
+    ws.close();
+
+  });
 
   ws.on('message', function message (data) {
-    console.log(data);
-    // to(char, data);
-    // to(hl3, data);
-    if (fs == null) {
-      fs.open(filename, 'w', function (err, file) {
-        if (err) throw err;
-        console.log('Open File!');
-      }); 
-    }
-    if (exp_con == "rmi") {
-          //RMI condition
-      if (user_id == laptop1 ) {
-            to(hl4, data)
-      }else if (user_id == laptop2) {
-            to(hl3, data)
-      }
+    console.log("Received from %s with musical data: %s ", user, data);
     
-    }else if (exp_con == "bmi") {
-          //BMI condition
-    if (user_id == hl3) {
-      to(hl4, data)
-    }else if (user_id == hl4) {
-      to(hl3, data)
-    }
-    }else if (exp_con == "localtest"){
-      to("::ffff:127.0.0.1", data)
-    }else if (exp_con == "hltest"){
-      to(hl3, data)
-    }else if (exp_con == "dostest"){
-      to("::ffff:192.168.0.216" ,data)
-    }else if  (exp_con == "duplex"){
-    //console.log(data)
-      if (user_id == laptop1) {
-      to(hl4, data)
-      }else if (user_id == laptop2) {
-      to(hl3, data)
-      }
-
-      if (user_id == hl4) {
-        to(hl3, data)
-      }else if (user_id == hl3) {
-        to(hl4, data)
-      }
-
-    }
+    // For AR-AI study.
+    genai_ws.send(data);
 
 
 
-// to(hl1, data)
-    // to(hl2, data)
-    // to("::ffff:192.168.0.216" ,data)
-    //console.log(`Received message ${data} from user ${user_id}`);
 
-    // let timeStamp = new Date();
-    // content = `Message ${data} from user @ ${user_id} at @ ${timeStamp}`;
-    //     fs.appendFile(filename.toString(), `${content}\n`, err => {
-    //       if (err) {
-    //         console.error(err);
-    //       }
-
-    //   });
+    // For previous AR study.
+    // if (fs == null) {
+    //   fs.open(filename, 'w', function (err, file) {
+    //     if (err) throw err;
+    //     console.log('Open File!');
+    //   }); 
+    // }
+    // if (exp_con == "rmi") {
+    //       //RMI condition
+    //   if (user_id == laptop1 ) {
+    //         to(hl4, data)
+    //   }else if (user_id == laptop2) {
+    //         to(hl3, data)
+    //   }
+    
+    // }else if (exp_con == "bmi") {
+    //       //BMI condition
+    // if (user_id == hl3) {
+    //   to(hl4, data)
+    // }else if (user_id == hl4) {
+    //   to(hl3, data)
+    // }
+    // }else if (exp_con == "localtest"){
+    //   to("::ffff:127.0.0.1", data)
+    // }else if (exp_con == "hltest"){
+    //   to(hl3, data)
+    // }else if (exp_con == "dostest"){
+    //   to("::ffff:192.168.0.216" ,data)
+    // }else if  (exp_con == "duplex"){
+    //   if (user_id == laptop1) {
+    //   to(hl4, data)
+    //   }else if (user_id == laptop2) {
+    //   to(hl3, data)
+    //   }
+    //   if (user_id == hl4) {
+    //     to(hl3, data)
+    //   }else if (user_id == hl3) {
+    //     to(hl4, data)
+    //   }
+    // }
 
   });
 
@@ -156,6 +203,4 @@ wss.on('connection', function(ws, request, client) {
 server.listen(port, function() {
    console.log(`Listening on http://localhost:${port}`);
 });
-
-
 
