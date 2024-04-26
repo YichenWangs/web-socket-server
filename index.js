b3fa17807b63134e568bea6efeb99d64a54488b8
yichen <yichen@voyager.local>
2024-04-26 12:54:19 +1000

    remove useless stuff for this branch

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const { createServer } = require('http');
 const WebSocket = require('ws');
 
-const uuid = require('uuid');  
+const uuid = require('uuid');
 const { timeStamp, log } = require('console');
 
 const fs = require('fs');
@@ -33,7 +33,7 @@ const ethernet3 = "::ffff:192.168.0.249";
 const charles_ai = "::ffff:127.0.0.1";
 
 var laptop1 = minilab1;
-var laptop2 = minilab2; 
+var laptop2 = minilab2;
 var hl1 = ethernet2;
 var hl2 = ethernet3;
 var hl3 = ethernet1;
@@ -42,64 +42,13 @@ var hl4 = ethernet2;
 
 
 const configured_control_nums_ai = [74, 71, 76, 77, 93, 18, 19, 16];
-const const_control_num_range = [[0, 1], [0, 1], [0, 1], [0, 1], [0,100],[0,100], [ -70, 12], [-70, 12]];
+const const_control_num_range = [[0, 1], [0, 1], [0, 1], [0, 1], [0, 100], [0, 100], [-70, 12], [-70, 12]];
 
 /**
  * Handy variables handling small things.
  */
 var filename;
 var exp_con;
-
-
-
-
-
-// /**
-//  * Websocket Client that connects to charles genai_midi_module.
-//  */
-// const genai_ws = new WebSocket('ws://localhost:5001');
-
-// genai_ws.on('open', function open() {
-//   console.log("Connection to charles's genAI server is open.");
-//   // Useless prompt message.
-//   genai_ws.send('/channel/0/noteoff/0/0');
-// });
-
-
-// genai_ws.on('message', function message(data) {
-//   //console.log('Received from the AI agent: %s', data);
-//   var parse_data = data.replace("cc", "controlchange");
-
-//   var route_data_from_ai_to_human = parse_data + "/ai";
-//   // Broadcast to hololens --- TODO: does this work?
-
-//     // testing AI to musician.
-//     // to(minilab1, genai_data);
-//     // to(hl3, genai_data);
-//     to(hl4, route_data_from_ai_to_human);
-//     // testing AI to Unity
-//     to("::ffff:192.168.0.216", route_data_from_ai_to_human);
-
-
-// });
-
-// genai_ws.on('error', function error(data) {
-//   console.log("Websocket error:" + data.code);
-//   genai_ws.close();
-  
-// });
-// genai_ws.on('close', function close(data) {
-//   console.log("The connection is closed.")
-//   console.log("Error information" + data);
-//   // setTimeout(() => {
-//   // }, 5000);
-// });
-
-
-
-// sockets[genai_ws.id] = genai_ws; // add to the list of socket
-
-
 
 const questions = [
   // {
@@ -132,17 +81,17 @@ inquirer.prompt(questions).then(answers => {
 // thanks to https://stackoverflow.com/questions/51316727/how-do-i-send-a-message-to-a-specific-user-in-ws-library.
 function to(user, data) {
 
-    if(sockets[user] && sockets[user].readyState === WebSocket.OPEN){
-      sockets[user].port = sockets_port[user];
-      sockets[user].send(data);
-      //console.log("Sending data to connection" + user + ":" + sockets[user].port ) ;
-    }
+  if (sockets[user] && sockets[user].readyState === WebSocket.OPEN) {
+    sockets[user].port = sockets_port[user];
+    sockets[user].send(data);
+    //console.log("Sending data to connection" + user + ":" + sockets[user].port ) ;
+  }
 }
 
 /**
  * Websocket Server on connection.
  */
-wss.on('connection', function(ws, request, client) {
+wss.on('connection', function (ws, request, client) {
   console.log("Client joined.");
   const remote_address = request.socket.remoteAddress;
   const remote_port = request.socket.remotePort;
@@ -160,16 +109,16 @@ wss.on('connection', function(ws, request, client) {
   //   console.log('File created!');
   // }); 
 
-  ws.on('error', function error (err){
+  ws.on('error', function error(err) {
     console.log(err.code);
     ws.close();
 
   });
 
-  ws.on('message', function message (data) {
+  ws.on('message', function message(data) {
     //console.log("Received from %s with musical data: %s ", user_id, data);
     const msg_array = data.split("/");
-    const tag = msg_array[msg_array.length-1];
+    const tag = msg_array[msg_array.length - 1];
     const channel_num = msg_array[2];
     const msg_type = msg_array[3];
     const control_num = msg_array[4];
@@ -180,114 +129,67 @@ wss.on('connection', function(ws, request, client) {
     var route_data_from_ai_to_human = data;
     var temp_tag_off = "";
 
-    for (let i = 0; i < msg_array.length-1; i++) {
-      temp_tag_off += msg_array[i]+ "/";
+    for (let i = 0; i < msg_array.length - 1; i++) {
+      temp_tag_off += msg_array[i] + "/";
     }
 
     // message comes from ai
     if (tag == "ai") {
 
-        // Handling data from AI agent sending to human musician Hololens.
-        //console.log("AI - Human:" + route_data_from_ai_to_human);
-        to("::ffff:192.168.0.216", route_data_from_ai_to_human);
-        to(hl3, route_data_from_ai_to_human);
-        to(hl4, route_data_from_ai_to_human);
+      // Handling data from AI agent sending to human musician Hololens.
+      //console.log("AI - Human:" + route_data_from_ai_to_human);
+      to("::ffff:192.168.0.216", route_data_from_ai_to_human);
+      to(hl3, route_data_from_ai_to_human);
+      to(hl4, route_data_from_ai_to_human);
 
-        
-    // message comes from human 
-    }else if (tag == "human") {
+
+      // message comes from human 
+    } else if (tag == "human") {
       // Handling data from human musician on keoboard sending to AI agent - change controlchange to cc
       // and to human musician Hololens.
       route_data_from_human_to_ai = temp_tag_off;
 
       //to(laptop1, route_data_from_human_to_ai);
-      if (msg_type == "controlchange" && configured_control_nums_ai.includes(control_num_int)){
+      if (msg_type == "controlchange" && configured_control_nums_ai.includes(control_num_int)) {
         route_data_from_human_to_ai = route_data_from_human_to_ai.replace("controlchange", "cc");
         console.log("Human - AI controlchange" + route_data_from_human_to_ai);
         //genai_ws.send(route_data_from_human_to_ai);
         to(charles_ai, route_data_from_human_to_ai);
-      }else if ((msg_type == "noteon" || msg_type == "noteoff") && (channel_num == 1)) {
+      } else if ((msg_type == "noteon" || msg_type == "noteoff") && (channel_num == 1)) {
         console.log("Human - AI note" + route_data_from_human_to_ai);
         //genai_ws.send(route_data_from_human_to_ai);
         to(charles_ai, route_data_from_human_to_ai);
-      }  
+      }
       //console.log("Human - Human" + route_data_from_human_to_human);
       to("::ffff:192.168.0.216", route_data_from_human_to_human);
       if (exp_con = "group") {
-              // send to each other
-          if (user_id == laptop1 ) {
+        // send to each other
+        if (user_id == laptop1) {
           to(hl3, route_data_from_human_to_human);
-          }else if (user_id == laptop2) {
+        } else if (user_id == laptop2) {
           to(hl4, route_data_from_human_to_human);
-          }
-    }
+        }
       }
-
-
+    }
 
     //var route_data_from_human_to_human = data + "/human";
     //console.log(route_data_from_human_to_ai)
     // console.log(route_data_from_human_to_human)  
 
-      // for testing in Unity
-      // var parse_data = data.replace("cc", "controlchange");
-      // to(hl4, parse_data);
-      // testing sending human musician information
+    // for testing in Unity
+    // var parse_data = data.replace("cc", "controlchange");
+    // to(hl4, parse_data);
+    // testing sending human musician information
 
-
-
-
-
-
-    // For previous AR study.
-    // if (fs == null) {
-    //   fs.open(filename, 'w', function (err, file) {
-    //     if (err) throw err;
-    //     console.log('Open File!');
-    //   }); 
-    // }
-    // if (exp_con == "rmi") {
-    //       //RMI condition
-    //   if (user_id == laptop1 ) {
-    //         to(hl4, data)
-    //   }else if (user_id == laptop2) {
-    //         to(hl3, data)
-    //   }
-    
-    // }else if (exp_con == "bmi") {
-    //       //BMI condition
-    // if (user_id == hl3) {
-    //   to(hl4, data)
-    // }else if (user_id == hl4) {
-    //   to(hl3, data)
-    // }
-    // }else if (exp_con == "localtest"){
-    //   to("::ffff:127.0.0.1", data)
-    // }else if (exp_con == "hltest"){
-    //   to(hl3, data)
-    // }else if (exp_con == "dostest"){
-    //   to("::ffff:192.168.0.216" ,data)x
-    // }else if  (exp_con == "duplex"){
-    //   if (user_id == laptop1) {
-    //   to(hl4, data)
-    //   }else if (user_id == laptop2) {
-    //   to(hl3, data)
-    //   }
-    //   if (user_id == hl4) {
-    //     to(hl3, data)
-    //   }else if (user_id == hl3) {
-    //     to(hl4, data)
-    //   }
-    // }
 
   });
 
-  ws.on('close', function() {
+  ws.on('close', function () {
     console.log("client left.");
   });
 });
 
 
-server.listen(port, function() {
-   console.log(`Listening on http://localhost:${port}`);
+server.listen(port, function () {
+  console.log(`Listening on http://localhost:${port}`);
 });
