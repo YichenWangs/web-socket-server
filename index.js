488c3bf891da4d72c74cd46ed8e1ba1378bdc1c3
Yichen Wang <yichenwang@Yichens-MacBook-Pro.local>
2024-04-26 12:55:50 +1000

    for cube project server

--- index.js
+++ index.js
@@ -44,44 +44,6 @@ var hl4 = ethernet2;
 var filename;
 var exp_con;
 
-var genai_data;
-
-/**
- * Websocket Client that connects to charles genai_midi_module.
- */
-const genai_ws = new WebSocket('ws://192.168.0.81:5001');
-
-genai_ws.on('open', function open() {
-  console.log("Connection o charles's genAI server is open.");
-  // Useless prompt message.
-  genai_ws.send('/channel/0/noteoff/0/0');
-});
-
-
-genai_ws.on('message', function message(data) {
-  console.log('Received from the AI agent: %s', data);
-  genai_data = data;
-  // Broadcast to hololens --- todo: does this work?
-  to(hl1, genai_data);
-  to(hl2, genai_data);
-
-
-});
-
-genai_ws.on('error', console.error);
-genai_ws.on('close', function close(data) {
-  console.log("The connection is closed.")
-  console.log("Error information" + data);
-  // setTimeout(() => {
-  //   connect();
-  // }, 1);
-});
-
-// sockets[genai_ws.id] = genai_ws; // add to the list of socket
-
-// genai_ws.on('message', route_message); // could do it this way so that
-
-// }
 
 
 const questions = [
@@ -126,6 +88,7 @@ wss.on('connection', function(ws, request, client) {
   const user_id = request.socket.remoteAddress;
   ws.id = user_id;
   sockets[ws.id] = ws;
+
   // console.log(sockets);
 
   // //create new file
@@ -135,7 +98,7 @@ wss.on('connection', function(ws, request, client) {
   // }); 
 
 
-
+const notes_map =  [ "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C5" ];
   ws.on('error', function error (err){
     console.log(err.code);
     ws.close();
@@ -144,53 +107,15 @@ wss.on('connection', function(ws, request, client) {
 
   ws.on('message', function message (data) {
     console.log("Received from %s with musical data: %s ", user_id, data);
-    
-    // For AR-AI study.
-    genai_ws.send(data);
+    console.log(data);
+    var msg = data.split('/');
+    var note = msg[1];
 
+    var idx = notes_map.indexOf(note);  
+    console.log(note);
+    var midi = 60 + idx;
 
-
-
-    // For previous AR study.
-    // if (fs == null) {
-    //   fs.open(filename, 'w', function (err, file) {
-    //     if (err) throw err;
-    //     console.log('Open File!');
-    //   }); 
-    // }
-    // if (exp_con == "rmi") {
-    //       //RMI condition
-    //   if (user_id == laptop1 ) {
-    //         to(hl4, data)
-    //   }else if (user_id == laptop2) {
-    //         to(hl3, data)
-    //   }
-    
-    // }else if (exp_con == "bmi") {
-    //       //BMI condition
-    // if (user_id == hl3) {
-    //   to(hl4, data)
-    // }else if (user_id == hl4) {
-    //   to(hl3, data)
-    // }
-    // }else if (exp_con == "localtest"){
-    //   to("::ffff:127.0.0.1", data)
-    // }else if (exp_con == "hltest"){
-    //   to(hl3, data)
-    // }else if (exp_con == "dostest"){
-    //   to("::ffff:192.168.0.216" ,data)
-    // }else if  (exp_con == "duplex"){
-    //   if (user_id == laptop1) {
-    //   to(hl4, data)
-    //   }else if (user_id == laptop2) {
-    //   to(hl3, data)
-    //   }
-    //   if (user_id == hl4) {
-    //     to(hl3, data)
-    //   }else if (user_id == hl3) {
-    //     to(hl4, data)
-    //   }
-    // }
+    console.log(midi);
 
   });
 
